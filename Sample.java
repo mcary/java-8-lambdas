b8f95b5fc2149ee5effa010e0083a5d0c99351da
Marcel M. Cary <marcel@oreilly.com>
2016-03-28 11:09:55 -0700

    Refactor Thread "Sample" to use a lambda

--- Sample.java
+++ Sample.java
@@ -1,10 +1,6 @@
 public class Sample {
     public static void main(String[] args) {
-        Thread th = new Thread(new Runnable() {
-            public void run() {
-                System.out.println("In another thread");
-            }
-        });
+        Thread th = new Thread(() -> System.out.println("In another thread"));
         th.start();
 
         System.out.println("In main");
