a4f0ca514bb08c0d89b774fd87b7953201d9068d
Marcel M. Cary <marcel@oreilly.com>
2016-03-28 12:06:19 -0700

    Avoid shared mutation by using "collect" terminator
    (The mutation is not shared across methods or threads, so it's not
    really an issue in this case... but let's pretend.)

--- Sample2.java
+++ Sample2.java
@@ -1,15 +1,14 @@
 import java.util.*;
+import static java.util.stream.Collectors.*;
 
 public class Sample2 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 1, 2, 3, 4, 5);
 
-        List<Integer> doubleOfEven = new ArrayList<>();
-
-        numbers.stream()
+        List<Integer> doubleOfEven = numbers.stream()
             .filter(e -> e % 2 == 0)
             .map(e -> e * 2)
-            .forEach(e -> doubleOfEven.add(e));
+            .collect(toList());
 
         System.out.println("Result: " + doubleOfEven);
     }
